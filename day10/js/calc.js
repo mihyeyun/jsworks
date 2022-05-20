0a372ec23d28a6599163d2dcb9fc2499a46e9b19
Your Name <you@example.com>
2022-05-20 18:18:32 +0900

    자바스크립트 소스 day10

---
+++ day10/js/calc.js
@@ -0,0 +1,11 @@
+// 덧샘 프로그램
+let n1 = parseInt(prompt("첫번째 수 입력 : "));
+let n2 = parseInt(prompt("두번째 수 입력 : "));
+let sum_v =0;
+
+//연산
+//입력받은 문자를 정수로 변환함
+sum_v = n1 + n2;
+
+//출력
+document.write("두 수의 합 : " + sum_v);
