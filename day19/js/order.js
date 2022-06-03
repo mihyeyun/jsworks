d79ea87b076d24f32ebdc528d7e04c29cd9a10b3
Your Name <you@example.com>
2022-06-03 15:49:28 +0900

    day19 소스파일

---
+++ day19/js/order.js
@@ -0,0 +1,23 @@
+let check = document.querySelector("#shippingInfo");
+check.addEventListener("click", checkbox);
+
+// let billingName = document.querySelector("#billingName");
+// let billingTel = document.querySelector("#billingTel");
+// let billingAddr = document.querySelector("#billingAddr");
+
+// let shippingName = document.querySelector("#shippingName");
+// let shippingTel = document.querySelector("#shippingTel");
+// let shippingAddr = document.querySelector("#shippingAddr");
+
+function checkbox(){
+    //name 속성으로 처리
+    if(check.checked==true){
+        shippingName.value = billingName.value;
+        shippingTel.value = billingTel.value;
+        shippingAddr.value = billingAddr.value;
+    }else{
+        shippingName.value = "";
+        shippingTel.value = "";
+        shippingAddr.value = "";
+    }
+}
